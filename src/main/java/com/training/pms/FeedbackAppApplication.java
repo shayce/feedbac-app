d87c26eb0458770e3ede6475517d1e1a19aba854
shaycesj <98834682+shaycesj@users.noreply.github.com>
2022-03-30 10:44:50 -0700

    first commit

---
+++ src/main/java/com/training/pms/FeedbackAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAppApplication.class, args);
+	}
+
+}
